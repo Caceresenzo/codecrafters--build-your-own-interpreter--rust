2267a0698d36e34ac4af151dcd8c735ab6ef3542
Caceresenzo <caceresenzo1502@gmail.com>
2024-07-07 15:56:14 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,198 @@
 use std::env;
+use std::fmt;
 use std::fs;
 use std::io::{self, Write};
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenType {
+    // Single character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier(String),
+    StringLiteral(String),
+    Number(f64),
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    // Other.
+    Eof,
+}
+
+// Implement the fmt::Display trait for TokenType
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TokenType::LeftParen => write!(f, "LEFT_PAREN"),
+            TokenType::RightParen => write!(f, "RIGHT_PAREN"),
+            TokenType::LeftBrace => write!(f, "LEFT_BRACE"),
+            TokenType::RightBrace => write!(f, "RIGHT_BRACE"),
+            TokenType::Comma => write!(f, "COMMA"),
+            TokenType::Dot => write!(f, "DOT"),
+            TokenType::Minus => write!(f, "MINUS"),
+            TokenType::Plus => write!(f, "PLUS"),
+            TokenType::Semicolon => write!(f, "SEMICOLON"),
+            TokenType::Slash => write!(f, "SLASH"),
+            TokenType::Star => write!(f, "STAR"),
+            TokenType::Bang => write!(f, "BANG"),
+            TokenType::BangEqual => write!(f, "BANG_EQUAL"),
+            TokenType::Equal => write!(f, "EQUAL"),
+            TokenType::EqualEqual => write!(f, "EQUAL_EQUAL"),
+            TokenType::Greater => write!(f, "GREATER"),
+            TokenType::GreaterEqual => write!(f, "GREATER_EQUAL"),
+            TokenType::Less => write!(f, "LESS"),
+            TokenType::LessEqual => write!(f, "LESS_EQUAL"),
+            TokenType::Identifier(_) => write!(f, "IDENTIFIER"),
+            TokenType::StringLiteral(_) => write!(f, "STRING"),
+            TokenType::Number(_) => write!(f, "NUMBER"),
+            TokenType::And => write!(f, "AND"),
+            TokenType::Class => write!(f, "CLASS"),
+            TokenType::Else => write!(f, "ELSE"),
+            TokenType::False => write!(f, "FALSE"),
+            TokenType::Fun => write!(f, "FUN"),
+            TokenType::For => write!(f, "FOR"),
+            TokenType::If => write!(f, "IF"),
+            TokenType::Nil => write!(f, "NIL"),
+            TokenType::Or => write!(f, "OR"),
+            TokenType::Print => write!(f, "PRINT"),
+            TokenType::Return => write!(f, "RETURN"),
+            TokenType::Super => write!(f, "SUPER"),
+            TokenType::This => write!(f, "THIS"),
+            TokenType::True => write!(f, "TRUE"),
+            TokenType::Var => write!(f, "VAR"),
+            TokenType::While => write!(f, "WHILE"),
+            TokenType::Eof => write!(f, "EOF"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, line: usize) -> Self {
+        Token {
+            token_type,
+            lexeme,
+            line,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let literal: String = match &self.token_type {
+            TokenType::Identifier(value) => value.into(),
+            TokenType::StringLiteral(value) => value.into(),
+            TokenType::Number(value) => value.to_string(),
+            _ => "null".into(),
+        };
+
+        write!(f, "{} {} {}", self.token_type, self.lexeme, literal)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Scanner {
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+    had_error: bool,
+}
+
+impl Scanner {
+    pub fn new(source: String) -> Self {
+        Scanner {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+            had_error: false,
+        }
+    }
+
+    pub fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    pub fn text(&self) -> String {
+        self.source[self.start..self.current].into()
+    }
+
+    pub fn scan_tokens(&mut self) -> Vec<Token> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        self.tokens.push(Token::new(TokenType::Eof, "".into(), self.line));
+        self.tokens.clone()
+    }
+
+    pub fn scan_token(&mut self) {
+        let character = self.advance();
+
+        match character {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            _ => (),
+        }
+    }
+
+    fn advance(&mut self) -> char {
+        let index = self.current;
+        self.current += 1;
+        self.source.chars().nth(index).unwrap()
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        self.tokens.push(Token::new(token_type, self.text(), self.line));
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,10 +211,11 @@ fn main() {
                 String::new()
             });
 
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            let mut scanner = Scanner::new(file_contents);
+            let tokens = scanner.scan_tokens();
+
+            for token in tokens {
+                println!("{}", token);
             }
         }
         _ => {
