a58dd8d592c1ce0fdcce3aa163fb2ffa989b72f9
Caceresenzo <caceresenzo1502@gmail.com>
2024-08-09 19:09:54 +0900

    pass 4th evaluating-expressions

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,4 @@
-use crate::{Expression, Literal};
+use crate::{Expression, Literal, TokenType};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Interpreter {}
@@ -18,7 +18,27 @@ impl Interpreter {
         match expression {
             Expression::Literal(literal) => Ok(literal),
             Expression::Grouping(child) => self.evaluate(*child),
+            Expression::Unary { operator, right } => {
+                let right_child = self.evaluate(*right)?;
+
+                match operator.token_type {
+                    TokenType::Bang => Ok(Literal::Boolean(!self.is_truthy(right_child))),
+                    TokenType::Minus => match right_child {
+                        Literal::Number(x) => Ok(Literal::Number(-x)),
+                        _ => Err(InterpreterError("expected number".into())),
+                    },
+                    _ => panic!("unreachable"),
+                }
+            }
             _ => Err(InterpreterError("unsupported".into())),
+        }
+    }
+
+    pub fn is_truthy(&self, literal: Literal) -> bool {
+        match literal {
+            Literal::Nil => false,
+            Literal::Boolean(value) => value,
+            _ => true,
         }
     }
 }
